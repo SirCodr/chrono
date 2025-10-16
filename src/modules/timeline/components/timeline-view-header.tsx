747ef82d16005bfdb014258982beab63d6057683
Juan Restrepo <juan.restrepooq@amigo.edu.co>
2025-10-16 16:59:01 -0500

    feat: implement useBreakpoint hook and integrate into TimelineViewHeader

--- src/modules/timeline/components/timeline-view-header.tsx
+++ src/modules/timeline/components/timeline-view-header.tsx
@@ -5,18 +5,20 @@ import { Plus } from "lucide-react"
 import { useState } from "react"
 import { RecordForm } from "./record-form"
 import { useTranslations } from "next-intl"
+import useBreakpoint from "@/hooks/useBreakpoint"
 
 export default function TimelineViewHeader() {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const { xs } = useBreakpoint()
   const t = useTranslations('timeline.activity')
 
   return (
     <>
       <div className="flex justify-between items-center">
         <h2 className="text-xl font-semibold">{t('title')}</h2>
-        <Button onClick={() => setIsModalOpen(true)} className="capitalize">
-          <Plus className="h-4 w-4 mr-2" />
-          {t('addRecord')}
+        <Button onClick={() => setIsModalOpen(true)} className="capitalize flex items-center">
+          <Plus className="h-4 w-4" />
+          {!xs && t('addRecord')}
         </Button>
       </div>
 
